a5df1312027f386ae83e6393f07254baa2536da0
pro-codeMaster <serenagrace0403@gmail.com>
2024-06-19 05:16:49 +0800

    init 101

---
+++ src/providers/session/session_provider.tsx
@@ -0,0 +1,13 @@
+import { useImmerReducer } from "use-immer";
+import { ReactNode } from "react";
+import { SessionContext, initialState } from './session_context'
+
+type SessionProviderProps = {
+  children: ReactNode;
+};
+
+export const SessionProvider = ({ children }: SessionProviderProps) => {
+  const { Provider } = SessionContext;
+  const [state, dispatch] = useImmerReducer(sessionReducer, initialState);
+  return <Provider value={{ state, dispatch }}>{children}</Provider>;
+};
