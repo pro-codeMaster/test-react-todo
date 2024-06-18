07d675c391fc07820162532d9abde5d034e2417e
pro-codeMaster <serenagrace0403@gmail.com>
2024-06-19 04:51:29 +0800

    store init

---
+++ src/providers/store/store.ts
@@ -0,0 +1,10 @@
+export const Vault = {
+  set: (key: string, value: string) => localStorage.setItem(key, value),
+  get: (key: string): string | null => localStorage.getItem(key),
+  remove: (key: string) => localStorage.removeItem(key),
+  clear: () => localStorage.clear(),
+  setObj: (key: string, value: Record<string, any>) =>
+    localStorage.setItem(key, JSON.stringify(value)),
+  getObj: <T>(key: string) =>
+    JSON.parse(localStorage.getItem(key) || "null") as T,
+};
